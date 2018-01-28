dcde6927b66405ad0e95d50b210630126067565e
Collins <collins.wekesa@andela.com>
2018-01-28 13:00:07 +0300

    troubleshooot gcloud cmd

--- deployment.sh
+++ deployment.sh
@@ -16,20 +16,20 @@ echo " Deploying to ${DEPLOYMENT_ENVIRONMENT}"
 if [ -z "$USE_CIRCLECI_BETA" ]; then
   # install kubectl and gcloud
   echo " Installing and configuring google cloud"
-  sudo /opt/google-cloud-sdk/bin/gcloud --quiet version
-  sudo /opt/google-cloud-sdk/bin/gcloud --quiet components update --version 120.0.0
-  sudo /opt/google-cloud-sdk/bin/gcloud --quiet components update --version 120.0.0 kubectl
+  sudo /home/circleci/google-cloud-sdk/bin/gcloud --quiet version
+  sudo /home/circleci/google-cloud-sdk/bin/gcloud --quiet components update --version 120.0.0
+  sudo /home/circleci/google-cloud-sdk/bin/gcloud --quiet components update --version 120.0.0 kubectl
 fi
 
 # set key and authenticate gcloud
 echo $GCLOUD_SERVICE_KEY | base64 --decode > ${HOME}/gcloud-service-key.json
-sudo /opt/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+sudo /home/circleci/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 
 # configure gcloud
-sudo /opt/google-cloud-sdk/bin/gcloud --quiet config set project $PROJECT_NAME
-sudo /opt/google-cloud-sdk/bin/gcloud --quiet config set container/cluster $CLUSTER_NAME
-sudo /opt/google-cloud-sdk/bin/gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
-sudo /opt/google-cloud-sdk/bin/gcloud --quiet container clusters get-credentials $CLUSTER_NAME
+sudo /home/circleci/google-cloud-sdk/bin/gcloud --quiet config set project $PROJECT_NAME
+sudo /home/circleci/google-cloud-sdk/bin/gcloud --quiet config set container/cluster $CLUSTER_NAME
+sudo /home/circleci/google-cloud-sdk/bin/gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
+sudo /home/circleci/google-cloud-sdk/bin/gcloud --quiet container clusters get-credentials $CLUSTER_NAME
 
 rm -rf ${DOCKER_SOURCE}
 mkdir -p ${DOCKER_SOURCE}
@@ -53,13 +53,13 @@ fi
 
 if [ -z "$USE_CIRCLECI_BETA" ]; then
   echo " Building image"
-  sudo /opt/google-cloud-sdk/bin/gcloud docker -- build -t gcr.io/${PROJECT_NAME}/${IMAGE}:$IMG_TAG  ${DOCKER_SOURCE} > /dev/null
+  sudo /home/circleci/google-cloud-sdk/bin/gcloud docker -- build -t gcr.io/${PROJECT_NAME}/${IMAGE}:$IMG_TAG  ${DOCKER_SOURCE} > /dev/null
   sudo docker tag -f gcr.io/${PROJECT_NAME}/${IMAGE}:${IMG_TAG} gcr.io/${PROJECT_NAME}/${IMAGE}:latest
   echo " Successfully built"
 
   echo " Pushing image"
-  sudo /opt/google-cloud-sdk/bin/gcloud docker push gcr.io/${PROJECT_NAME}/${IMAGE}:$IMG_TAG
-  sudo /opt/google-cloud-sdk/bin/gcloud docker push gcr.io/${PROJECT_NAME}/${IMAGE}:latest
+  sudo /home/circleci/google-cloud-sdk/bin/gcloud docker push gcr.io/${PROJECT_NAME}/${IMAGE}:$IMG_TAG
+  sudo /home/circleci/google-cloud-sdk/bin/gcloud docker push gcr.io/${PROJECT_NAME}/${IMAGE}:latest
   echo " Successfully pushed"
   sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 else

8c8986d43c27dd8fb17909b02355ad0e16ab619e
Collins <collins.wekesa@andela.com>
2018-01-28 15:43:31 +0300

    troubleshoot permission issue

--- deployment.sh
+++ deployment.sh
@@ -24,6 +24,7 @@ fi
 # set key and authenticate gcloud
 echo $ACCOUNT_ID_KEY | base64 --decode > ${HOME}/gcloud-service-key.json
 sudo /opt/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+export GOOGLE_APPLICATION_CREDENTIALS=${HOME}/gcloud-service-key.json
 # sudo /opt/google-cloud-sdk/bin/gcloud beta auth application-default activate-service-account --key-file ${HOME}/gcloud-service-key.json
 
 # configure gcloud

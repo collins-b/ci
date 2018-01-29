732eaaaee39328f6c42f26c195cc1854d4188074
Collins <collins.wekesa@andela.com>
2018-01-29 13:41:12 +0300

    refactor code

--- deployment.sh
+++ deployment.sh
@@ -1,80 +1,27 @@
 # !/bin/bash
 
 set -e
-export IMG_TAG=$(echo $CIRCLE_SHA1 | cut -c -7)
-
-[ -z "$DOCKER_PROJECT_NAME" ] && echo "Docker github repository name must be set in env as DOCKER_PROJECT_NAME ";
-
-APPLICATION_SRC=$HOME/$CIRCLE_PROJECT_REPONAME/
-DOCKER_SOURCE=$HOME/docker-src
-
-DEPLOYMENT_ENVIRONMENT="staging"
-
-echo " Deploying to ${DEPLOYMENT_ENVIRONMENT}"
-
-  # install kubectl and gcloud
-echo " Installing and configuring google cloud"
-sudo /opt/google-cloud-sdk/bin/gcloud --quiet version
-sudo /opt/google-cloud-sdk/bin/gcloud --quiet components update --version 120.0.0
-sudo /opt/google-cloud-sdk/bin/gcloud --quiet components update --version 120.0.0 kubectl
-
-# set key and authenticate gcloud
-echo $ACCOUNT_KEY_STAGING | base64 --decode > ${HOME}/gcloud-service-key.json
-sudo /opt/google-cloud-sdk/bin/gcloud auth activate-service-account ${ACCOUNT_ID} --key-file ${HOME}/gcloud-service-key.json
-# sudo /opt/google-cloud-sdk/bin/gcloud beta auth application-default activate-service-account --key-file ${HOME}/gcloud-service-key.json
-
-# configure gcloud
-sudo /opt/google-cloud-sdk/bin/gcloud --quiet config set project $PROJECT_NAME
-sudo /opt/google-cloud-sdk/bin/gcloud --quiet config set container/cluster $CLUSTER_NAME
-sudo /opt/google-cloud-sdk/bin/gcloud --quiet config set compute/zone $CLOUDSDK_COMPUTE_ZONE
-sudo /opt/google-cloud-sdk/bin/gcloud --quiet container clusters get-credentials $CLUSTER_NAME
-
-rm -rf ${DOCKER_SOURCE}
-mkdir -p ${DOCKER_SOURCE}
-
-# pull docker repo
-echo " Pulling docker image source from git "
-/usr/bin/git clone --depth=1 -b ${TAG:-dockerize-app} git@github.com:collins-b/${DOCKER_PROJECT_NAME}.git ${DOCKER_SOURCE}
-echo " Successfully pulled"
-
-mkdir ${DOCKER_SOURCE}/app/
-
-echo " Injecting values from machine env"
-DOCKER_FILE_CONTENT=`perl -p -e 's/\%([^%]+)\%/defined $ENV{$1} ? $ENV{$1} : "\${$1}"/eg' ${DOCKER_SOURCE}/Dockerfile`
-echo "$DOCKER_FILE_CONTENT" > ${DOCKER_SOURCE}/Dockerfile
-echo " Successfully injected"
-
-echo " Building image"
-if ! [ -z "$CIRCLE_TAG" ]; then
-  IMG_TAG="$CIRCLE_TAG"
-fi
-
-# if [ -z "$USE_CIRCLECI_BETA" ]; then
-#   echo " Building image"
-#   sudo /opt/google-cloud-sdk/bin/gcloud docker -- build -t gcr.io/${PROJECT_NAME}/${IMAGE}:$IMG_TAG  ${DOCKER_SOURCE} > /dev/null
-#   sudo docker tag -f gcr.io/${PROJECT_NAME}/${IMAGE}:${IMG_TAG} gcr.io/${PROJECT_NAME}/${IMAGE}:latest
-#   echo " Successfully built"
-
-#   echo " Pushing image"
-#   sudo /opt/google-cloud-sdk/bin/gcloud docker push gcr.io/${PROJECT_NAME}/${IMAGE}:$IMG_TAG
-#   sudo /opt/google-cloud-sdk/bin/gcloud docker push gcr.io/${PROJECT_NAME}/${IMAGE}:latest
-#   echo " Successfully pushed"
-#   sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-# else
-  echo " Building image"
-  gcloud docker -- build -t gcr.io/${PROJECT_NAME}/${IMAGE}:$IMG_TAG  ${DOCKER_SOURCE} > /dev/null
-  gcloud docker -- tag gcr.io/${PROJECT_NAME}/${IMAGE}:${IMG_TAG} gcr.io/${PROJECT_NAME}/${IMAGE}:latest
-  echo " Successfully built"
-
-  echo " Pushing image"
-  gcloud docker -- push gcr.io/${PROJECT_NAME}/${IMAGE}:$IMG_TAG
-  gcloud docker -- push gcr.io/${PROJECT_NAME}/${IMAGE}:latest
-  echo " Successfully pushed"
-#fi
-
-# TODO: push to general access repo too
-
-echo " Deploying to ${DEPLOYMENT_ENVIRONMENT}"
-/opt/google-cloud-sdk/bin/kubectl config current-context
-/opt/google-cloud-sdk/bin/kubectl set image deployment/${DEPLOYMENT} ${CONTAINER_NAME}=gcr.io/${PROJECT_NAME}/${IMAGE}:$IMG_TAG
-echo " Successfully deployed to ${DEPLOYMENT_ENVIRONMENT} :)"
+# Save the string to a text file
+echo "Deploying to ${DEPLOYMENT_ENVIRONMENT}"
+- echo $ACCOUNT_KEY_STAGING > key.txt
+        # Decode the Service Account
+- base64 -i key.txt -d > ${HOME}/gcloud-service-key.json
+        # Authenticate CircleCI with the service account file
+- sudo /opt/google-cloud-sdk/bin/gcloud auth activate-service-account ${ACCOUNT_ID} --key-file ${HOME}/gcloud-service-key.json
+        # Set the default project
+- sudo /opt/google-cloud-sdk/bin/gcloud config set project $PROJECT_ID
+        # Set the default container
+- sudo /opt/google-cloud-sdk/bin/gcloud --quiet config set container/cluster $CLUSTER_NAME
+        # Set the compute zone
+- sudo /opt/google-cloud-sdk/bin/gcloud config set compute/zone $CLOUDSDK_COMPUTE_ZONE
+        # Get the cluster credentials.
+- sudo /opt/google-cloud-sdk/bin/gcloud --quiet container clusters get-credentials $CLUSTER_NAME
+        # Start good old Docker
+- sudo service docker start
+        # Build a docker image and use the Github commit hash ($CIRCLE_SHA1) as the tag
+- docker build -t gcr.io/${PROJECT_ID}/${REG_ID}/edms:$CIRCLE_SHA1 .
+        # Push the Image to the GCP Container Registry
+- sudo /opt/google-cloud-sdk/bin/gcloud docker -- push gcr.io/${PROJECT_ID}/${REG_ID}/edms:$CIRCLE_SHA1
+        # Update the default image for the deployment
+- sudo /opt/google-cloud-sdk/bin/kubectl set image deployment/${DEPLOYMENT_NAME} ${CONTAINER_NAME}=gcr.io/${PROJECT_ID}/${REG_ID}/edms:$CIRCLE_SHA1
+echo " Successfully deployed to ${DEPLOYMENT_ENVIRONMENT}"
